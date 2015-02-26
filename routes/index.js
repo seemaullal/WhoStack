2e90bd332db173f8a0f2785c63f1e9df83f66b57
Sarah Muenzinger <sarah.muenzinger@gmail.com>
2015-02-26 11:49:45 -0500

    groups are good to go on the backend

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ var models = require("../models/index");
 router.get('/', function(req, res, next) {
 	res.sendFile('./index.html', {root: './'});
 });
+var token;
 router.get('/game', function(req, res, next) {
 
 	var url = 'https://slack.com/api/oauth.access';
@@ -16,17 +17,19 @@ router.get('/game', function(req, res, next) {
 	request({url: url, qs: qs }, function (error, response, body) {
 		if (!error && response.statusCode == 200) {
 			var body = JSON.parse(body);
-			var token = body.access_token;
+			token = body.access_token;
 			console.log("token", token);
 			request({url: "https://slack.com/api/auth.test", qs: {token: token} }, function (error, response, body) {
 				if (!error && response.statusCode == 200) {
 					console.log("body part 2",body);
 					body = JSON.parse(body);
 					var user_id = body.user_id;
-					models.User.fundONe({UserId: user_id}, function(err, user){
+					models.User.findOne({UserId: user_id}, function(err, user){
 						if (err) console.log(err);
 						// find an existing user from the database
-						else if (user) console.log(user);
+						else if (user) {
+							console.log(user);
+						}
 						else {
 							models.User.create({ UserId: user_id, Token: token});
 						}
@@ -38,6 +41,18 @@ router.get('/game', function(req, res, next) {
 	res.sendFile('./game.html', {root: './'});
 });
 
+router.get("/groups", function(req, res, next){
+	//make a call to slack's api for channels.list with token (how do we now find the right token from our model?)
+	//that will return an object that contains array of channel objects
+	//we will then send on our own page a 
+	url = "https://slack.com/api/groups.list" 
+	qs = {token: token}
+	request({url: url, qs: qs }, function (error, response, body) {
+		body = JSON.parse(body);
+		console.log(body)
+		res.send(body)
+	})
+})
 router.get('/login' , function(req, res, next) {
 	req.redirect_uri = 'http://127.0.0.1:3000/game';
 	res.redirect('https://slack.com/oauth/authorize?client_id='+ config.slackSecrets.client_id + '&redirect_uri=' +  req.redirect_uri + '&state=' + config.slackSecrets.state);
