577b15d03268c630077dbd52a403dd1ebd78f6fb
Seema <seemaullal@gmail.com>
2015-02-27 14:43:11 -0500

    Third Heroku attempt

---
+++ config.js
@@ -0,0 +1,8 @@
+var slackSecrets = {
+	client_id: "2151814398.3814951849",
+    client_secret: "ae8d1ddc9a5702c6c8d22f7902ad3efb",
+    state: 'passportsucks'
+};
+module.exports = {
+	slackSecrets: slackSecrets
+};
---
+++ notes.js
@@ -0,0 +1,9 @@
+/**********************
+Have the group select
+For each member in the group, 
+	get the user info for that member
+
+
+
+
+*********************/
