e5bb986cc2723401a72b6fdea16b245931b9946c
Seema <seemaullal@gmail.com>
2015-02-28 01:28:34 -0500

    Removed logging with morgan for now

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var app = express();
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
+// app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
