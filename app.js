684c2ebc2f42c5c569e06e289c5b9c2eb1da17ee
Seema <seemaullal@gmail.com>
2015-02-28 01:37:17 -0500

    Removed logging with morgan for now

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(session({
     secret: 'trumpets',
+    saveUninitialized: false,
 }));
 
 app.use(express.static(path.join(__dirname, 'public')));
