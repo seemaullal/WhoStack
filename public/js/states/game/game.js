0eaf48418c0b49ddea7dd84daf201c3db42c3a5d
Sarah Muenzinger <sarah.muenzinger@gmail.com>
2015-04-21 15:53:03 -0400

    game feature seems working if ugly

--- public/js/states/game/game.js
+++ public/js/states/game/game.js
@@ -18,7 +18,7 @@ app.config(function ($stateProvider) {
   });
 });
 
-app.controller('GameCtrl', function ($scope) {
+app.controller('GameCtrl', function ($scope, GroupFactory, MemberFactory) {
   //potential game logic:
   //we click start
   //a random person is selected and subtracted from the guessing pool
@@ -46,17 +46,35 @@ app.controller('GameCtrl', function ($scope) {
   };
   $scope.start = function(){
     //we click start
-    $scope.randomPick();
+    $scope.gameMembers = $scope.members.slice()
+    $scope.randomPick($scope.gameMembers);
+    $scope.guesses = 5;
+    $scope.pointsFromGame = 0;
   }
-  $scope.randomPick = function(){
+  $scope.randomPick = function(people){
     //a random person is selected and subtracted from the guessing pool
     //display that person to the user
+    $scope.randomPerson = people[Math.floor(Math.random()*people.length)];
   }
-    $scope.photoClicked = function(member, $index) {
+  $scope.photoClicked = function(member, $index) {
     $scope.selectedIndex = $index;
     $scope.name = member.real_name;
     //on click of a person it tells us who the person is and if they are the random person
-    //if they are not the random person you lose a life
-    //if they are the random person you get points and a new person is randomly chosen
+    if($scope.name === $scope.randomPerson.user.profile.real_name){
+      //if they are the random person you get points
+      $scope.pointsFromGame += 10;
+      //they are removed from the guessing pool (backend only)
+      _.remove($scope.gameMembers, function(person) {
+        return person.user.profile.real_name === $scope.name
+      });
+      //newrando is chosen after 3 seconds or something
+      $scope.randomPick($scope.gameMembers)
+    }else{
+      //if they are not the random person you lose a life
+      $scope.guesses--;
+    }
+    if($scope.gameMembers.length === 0){
+      $scope.message = "YOU WON! WITH "+$scope.pointsFromGame +" points and " + $scope.guesses+"gueses left!"
+    }
   };
 });
